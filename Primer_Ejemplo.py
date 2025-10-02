a1e5f37de4e2b977ada27f224d17c4e98acf562c
a01770616-prog <a01770616@tec.mx>
2025-10-01 18:17:18 -0600

    Creación de Rama 1

--- Primer_Ejemplo.py
+++ Primer_Ejemplo.py
@@ -13,3 +13,24 @@ print(suma(5, 3))
 def suma(a, b, c):
     print(a + b + c)
     suma()
+
+def generar_datos():
+    dias = [1, 2, 3, 4, 5]
+    ventas = [100, 150, 200, 250, 300]
+    return list(dias, ventas)
+
+def resumen_analitica():
+    dias,ventas = generar_datos()
+    total = sum(ventas)
+    promedio = total / len(ventas)
+
+    return {
+        "dias": dias,
+        "ventas": ventas,
+        "total": total,
+        "promedio": promedio
+    }
+
+datos = resumen_analitica()
+print("Total de ventas:", datos["total"])
+print("Promedio de ventas:", datos["promedio"])
