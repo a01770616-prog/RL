1c49a7c7c6ab121785a148fe6ec5d5bcb2edb18a
a01770616-prog <a01770616@tec.mx>
2025-10-01 18:44:34 -0600

    Creacion de funciones Front

--- Primer_Ejemplo.py
+++ Primer_Ejemplo.py
@@ -13,3 +13,22 @@ print(suma(5, 3))
 def suma(a, b, c):
     print(a + b + c)
     suma()
+
+import matplotlib.pyplot as plt
+
+def graficar_ventas(dias,ventas):
+    plt.figure(figsize=(10,5))
+    plt.plot(dias, ventas,marker='o', linestyle='-',color='blue')
+    plt.title('Gráfica de Ventas diarias')
+    plt.xlabel('Días')
+    plt.ylabel('Ventas')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+def main_frontend():
+    dias = [1, 2, 3, 4, 5, 6, 7]
+    ventas = [150, 200, 250, 300, 350, 400, 450]
+    graficar_ventas(dias, ventas)   
+
+main_frontend()
